3243e7dc79620465d8372f1514bd8657a0a5f986
cyrusDev1 <cyrushoungue1240@gmail.com>
2022-11-18 12:29:17 +0100

    building

--- vite.config.js
+++ vite.config.js
@@ -6,14 +6,21 @@ import babel from 'vite-plugin-babel';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      "/api": {
+        target: "https://world-cup-json-2022.fly.dev",
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
   plugins: [vue(), babel()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  devServer: {
-    proxy: 'https://world-cup-json-2022.fly.dev',
-}
 });
 
