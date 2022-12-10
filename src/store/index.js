b7d3ee0e5129e582b12d6dea3de257c3b0379415
cyrusDev1 <cyrushoungue1240@gmail.com>
2022-12-10 13:07:16 +0100

    new update

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,5 @@
 const req = axios.create({
-  baseURL: 'https://whispering-sands-46177.herokuapp.com/https://world-cup-json-2022.fly.dev',
+  baseURL: 'https://world-cup-json-2022.fly.dev',
 });
 
 export default req;
