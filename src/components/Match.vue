1ff82fdf36eda98a544c5546581ae9d0e8a5f55d
cyrusDev1 <cyrushoungue1240@gmail.com>
2022-12-10 13:22:38 +0100

    new update

--- src/components/Match.vue
+++ src/components/Match.vue
@@ -9,14 +9,14 @@
                     <span v-if="match.stage_name == 'Round of 16'">{{ match.home_team.country }}</span>
                     <span v-else >{{ match.home_team.name == 'To Be Determined' ? "TBD" : match.home_team.name }}</span>
                 </div>
-                <img v-if="match.status == 'future_scheduled'" :src="getFlag(match.home_team.country)" class="home-flag" width="20" height="15">
+                <img v-if="match.status == 'future_scheduled' || match.status == 'completed'" :src="getFlag(match.home_team.country)" class="home-flag" width="20" height="15">
             </div>
             <div  class="sc">
                 <span v-if="match.status != 'future_scheduled'" class="d-block">{{ match.home_team.goals }} : {{ match.away_team.goals }}</span>
                 <span v-else class="d-block"> vs </span>
             </div>
             <div>
-                <img  v-if="match.status == 'future_scheduled'" :src="getFlag(match.away_team.country)" class="away-flag" width="20" height="15">  
+                <img v-if="match.status == 'future_scheduled' || match.status == 'completed'" :src="getFlag(match.away_team.country)" class="away-flag" width="20" height="15">  
                 <div class="away">
                     <span v-if="match.stage_name == 'Round of 16'" >{{ match.away_team.country }}</span>
                     <span v-else >{{ match.away_team.name == 'To Be Determined' ? "TBD" : match.away_team.name }}</span>
